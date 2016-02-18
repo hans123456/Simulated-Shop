3d0001bbafec2ab7bdab2c95f29f84a550968913
Student <Student@G302BPC08.manila.dlsu.edu.ph>
2016-02-18 15:41:19 +0800

    added adminScreen to driver

--- src/Driver.java
+++ src/Driver.java
@@ -5,18 +5,23 @@ import javax.swing.JFrame;
 
 import ui.MainFrame;
 
-
 public class Driver {
 
 	public static void main(String[] args) {
-		
+
+		ShopDatabase shopDB = new ShopDatabase();
+		AdminScreen adminScreen = new AdminScreen();
+		adminScreen.setShopDB(shopDB);
+		adminScreen.setVisible(true);
+
 		JFrame frame = new MainFrame();
-		
+
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(Toolkit.getDefaultToolkit().getScreenSize().width, Toolkit.getDefaultToolkit().getScreenSize().height);
+		frame.setSize(Toolkit.getDefaultToolkit().getScreenSize().width,
+				Toolkit.getDefaultToolkit().getScreenSize().height);
 		frame.setResizable(false);
 		frame.setTitle("SHOPPING!");
 		frame.setVisible(true);
 	}
-	
+
 }
